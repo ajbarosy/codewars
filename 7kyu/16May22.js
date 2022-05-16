27c1e529756b5749de6ae2985d2a3ada4c217a67
ajbarosy <ajbarosy@gmail.com>
2022-05-16 13:51:26 -0400

    Beginner Series #3 Sum of Numbers from 16 May 22

---
+++ 7kyu/16May22.js
@@ -0,0 +1,25 @@
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b. Note: a and b are not ordered!
+
+// Examples: 
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+
+
+function getSum( a, b){
+  
+    //   let sum = a + b
+      
+    //   if (a == b){
+    //     return a 
+    //   }
+      
+    //     return sum 
+    
+    // }
+    
+    let list = [];
+    for (let i = a; i <= b; i++) {
+      console.log(  list.push(i) )
+    }
+    // He wants you to take -359 + 130 + -229 + -228 + -227... that would equal the -56105 Hope this helps :)
