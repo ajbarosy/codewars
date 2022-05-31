00f8fc3ae495455a5a2e487e7bf00c588ad24718
ajbarosy <ajbarosy@gmail.com>
2022-05-30 20:15:33 -0400

    Function 2 - squaring an argument from 30 May 2022

---
+++ 8kyu/30May22.js
@@ -0,0 +1,10 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+function square (x){
+  
+    return Math.pow(x, 2)
+  }
+
+  //or
+
+  const square = s => x * x;
