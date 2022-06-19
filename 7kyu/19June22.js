c1cfedecf171f51989031971a4325e6f6596ea6a
ajbarosy <ajbarosy@gmail.com>
2022-06-19 18:50:46 -0400

    modification

--- 7kyu/19June22.js
+++ 7kyu/19June22.js
@@ -14,45 +14,45 @@ The most frequently used key is "GA-DE-RY-PO-LU-KI".
 */
 
 function encode(str) {
-    let arr = [...str];
-    let result = arr.map(e => {
-      switch(e) {
-          case 'G': return 'A';
-          case 'g': return 'a';
-          case 'A': return 'G';
-          case 'a': return 'g';
-          
-          case 'D': return 'E';
-          case 'd': return 'e';
-          case 'E': return 'D';
-          case 'e': return 'd';
-          
-          case 'R': return 'Y';
-          case 'r': return 'y';
-          case 'Y': return 'R';
-          case 'y': return 'r';
-          
-          case 'P': return 'O';
-          case 'p': return 'o';
-          case 'O': return 'P';
-          case 'o': return 'p';
-          
-          case 'L': return 'U';
-          case 'l': return 'u';
-          case 'U': return 'L';
-          case 'u': return 'l';
-          
-          case 'K': return 'I';
-          case 'k': return 'i';
-          case 'I': return 'K';
-          case 'i': return 'k';
-          
-          default: return e;
-      }
-    });
-    return result.join('');
-  }
-  
-  function decode(str) {
-    return encode(str);
-  }
+  let arr = [...str];
+  let result = arr.map(e => {
+    switch(e) {
+        case 'G': return 'A';
+        case 'g': return 'a';
+        case 'A': return 'G';
+        case 'a': return 'g';
+        
+        case 'D': return 'E';
+        case 'd': return 'e';
+        case 'E': return 'D';
+        case 'e': return 'd';
+        
+        case 'R': return 'Y';
+        case 'r': return 'y';
+        case 'Y': return 'R';
+        case 'y': return 'r';
+        
+        case 'P': return 'O';
+        case 'p': return 'o';
+        case 'O': return 'P';
+        case 'o': return 'p';
+        
+        case 'L': return 'U';
+        case 'l': return 'u';
+        case 'U': return 'L';
+        case 'u': return 'l';
+        
+        case 'K': return 'I';
+        case 'k': return 'i';
+        case 'I': return 'K';
+        case 'i': return 'k';
+        
+        default: return e;
+    }
+  });
+  return result.join('');
+}
+
+function decode(str) {
+  return encode(str);
+}
