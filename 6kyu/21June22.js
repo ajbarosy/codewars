c6acdcf369c97e1538a19e4f7747574f76a7668c
ajbarosy <ajbarosy@gmail.com>
2022-06-21 13:19:11 -0400

    Your order, please from 21 June 2022

---
+++ 6kyu/21June22.js
@@ -0,0 +1,20 @@
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+*/
+
+function order(words){
+    var array = words.split(' ');
+    var sortedArray = [];
+    for(i = 0; i <= array.length; i++) {
+      for(j = 0; j < array.length; j++) {
+        if(array[j].indexOf(i) >= 0) {
+          sortedArray.push(array[j]);
+        }
+      }
+    }
+    return sortedArray.join(' ');
+  }
