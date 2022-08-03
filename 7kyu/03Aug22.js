3f3884c773ba2b0c33daf2306bcf703f9794d1f2
ajbarosy <ajbarosy@gmail.com>
2022-08-03 15:38:13 -0400

    Count the divisors of a number from 03 August 2022

---
+++ 7kyu/03Aug22.js
@@ -0,0 +1,17 @@
+/*
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+*/
+
+function getDivisorsCnt(n){
+    let numbers = 0;
+    
+    for(let i = 1; i <= n; i++){
+      if(n % i == 0){
+        numbers += 1
+        
+      }
+    }
+    return numbers
+  }
