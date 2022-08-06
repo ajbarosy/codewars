1e1b0cfeb7d0381ea96c8ba0e307887404b966ac
ajbarosy <ajbarosy@gmail.com>
2022-08-06 11:06:49 -0400

    Keep up the hoop from 06 August 2022

--- 8kyu/06Aug22.js
+++ 8kyu/06Aug22.js
@@ -1,5 +1,14 @@
-//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+/*
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
 
-function reverseList(list) {
-    return list.reverse()
-    }
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+*/
+
+function hoopCount (n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+ 
+ }
