df2619360c72ac31709d5a81d2da7fd76e3d055b
ajbarosy <ajbarosy@gmail.com>
2022-08-17 16:24:09 -0400

    Exclamation marks series #1: Remove an exclamation mark from the end of string from 17 August 2022

---
+++ 8kyu/17Aug22.js
@@ -0,0 +1,8 @@
+//Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+function remove (s) {
+  
+    return  s[s.length -1] == '!' ? s.slice(0, -1) : s
+    
+    }
+    
