07ae5c33cf024078decdbe31b0734528575954fb
ajbarosy <ajbarosy@gmail.com>
2022-09-18 20:48:24 -0400

    Remove anchor from URL from 18 September 2022

---
+++ 7kyu/18Sept22.js
@@ -0,0 +1,13 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    if (url.includes('#')){
+      return url.slice(0, url.indexOf('#')) 
+      }else{
+        return url
+      }
+  }
