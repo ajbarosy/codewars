8301596ac65c49842d999259f8c6d6ebb1a1d738
ajbarosy <ajbarosy@gmail.com>
2022-09-27 23:06:34 -0400

    Strings: swap vowels' case from 27 September 2022

---
+++ 7kyu/27Sept22.js
@@ -0,0 +1,23 @@
+// Challenge:
+
+// Given a string, return a copy of the string with the vowels' case swapped.
+
+// For this kata, assume that vowels are in the set "aeouiAEOUI".
+
+// Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+// Addendum: Your solution is only required to work for the ASCII character set.
+
+// Please make sure you only swap cases for the vowels.
+
+function swapVowelCase(str) {
+    return [...str].map((letter,i)=>{
+      if("aeoiu".includes(letter)){
+        return letter.toUpperCase()
+      }
+      else if("AEOIU".includes(letter)){
+         return letter.toLowerCase()
+      }
+       return letter;
+    }).join("")
+  }
