9ad29dbf41c22b5335496fb593d625879bed3f8a
ajbarosy <ajbarosy@gmail.com>
2022-10-19 20:26:44 -0400

    Simple Fun #176: Reverse Letter

---
+++ 7kyu/19Oct22.js
@@ -0,0 +1,12 @@
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+function reverseLetter(str) {
+    let reversedString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i].toLowerCase() === str[i].toUpperCase()) {
+            continue;
+        }
+        reversedString += str[i];
+    }
+    return reversedString;
+}
